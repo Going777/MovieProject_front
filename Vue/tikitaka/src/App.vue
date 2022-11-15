02a1c94353b77aa095845d3558dd5faf5bae36e6
seoda0000 <seoda0000@gmail.com>
2022-11-15 15:45:14 +0900

    nav 작업 시작

--- Vue/tikitaka/src/App.vue
+++ Vue/tikitaka/src/App.vue
@@ -1,5 +1,40 @@
 <template>
   <v-app>
+    <!-- 최상단 nav bar -->
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+    
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+    
+      <v-spacer></v-spacer>
+    
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <!-- 왼쪽 nav bar -->
     <v-navigation-drawer permanent expand-on-hover fixed>
       <v-list nav dense>
         <v-list-item link>
@@ -23,42 +58,12 @@
       </v-list>
     </v-navigation-drawer>
 
+
+    <!-- 메인 화면 -->
     <v-main style="margin-left: 75px; margin-top: 15px">
       <router-view />
     </v-main>
 
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
   </v-app>
 </template>
 
