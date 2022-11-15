d9177a06cff4f60ba0c191f4f2fa8ea02975f4c5
JW <wjddnjs0528@gmail.com>
2022-11-15 21:55:02 +0900

    유튜브 영상 무한재생

--- Vue/tikitaka/src/components/MovieVideoListItem.vue
+++ Vue/tikitaka/src/components/MovieVideoListItem.vue
@@ -20,7 +20,8 @@ export default {
   },
   computed: {
     videoPath() {
-      return `https://www.youtube.com/embed/${this.movie.key}?autoplay=1&mute=1&rel=0&showsearch=0&color=white`
+      // 유튜브 무한 반복 재생
+      return `https://www.youtube.com/embed/${this.movie.key}?autoplay=1&mute=1&rel=0&showsearch=0&controls=0&fs=0&loop=1&modestbranding=1&loop=1&playlist=${this.movie.key}`
     },
   },
 }
