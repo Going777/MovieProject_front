046179c18dc9102b9ebc02560ea595f70cc416cf
Going777 <wjddnjs0528@gmail.com>
2022-11-15 15:39:44 +0900

    네비게이션 추가

--- Vue/tikitaka/src/App.vue
+++ Vue/tikitaka/src/App.vue
@@ -1,5 +1,32 @@
 <template>
   <v-app>
+    <v-navigation-drawer permanent expand-on-hover fixed>
+      <v-list nav dense>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-folder</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>My Files</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-account-multiple</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Shared with me</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-star</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Starred</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main style="margin-left: 75px; margin-top: 15px">
+      <router-view />
+    </v-main>
+
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
@@ -32,17 +59,12 @@
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
-
-    <v-main>
-      <router-view />
-    </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
   name: "App",
-
   data: () => ({
     //
   }),
