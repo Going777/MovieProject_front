18e5200a1295ef54473aacd5c9709b373e48a60c
Going777 <wjddnjs0528@gmail.com>
2022-11-17 12:15:48 +0900

    detail 페이지 눌렀을 때 화면 이동 로직 구현

--- Vue/tikitaka/src/views/DetailView.vue
+++ Vue/tikitaka/src/views/DetailView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="detail-container">
     <p>{{ movie }}</p>
   </div>
 </template>
@@ -18,4 +18,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.detail-container {
+  background-image: ;
+}
+</style>
