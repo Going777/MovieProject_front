609cd1ab8c3d306c9bdf1ff56bcbbf5c47760ede
JW <wjddnjs0528@gmail.com>
2022-11-18 13:06:09 +0900

    프로필 섹션 레이아웃 정리

--- Vue/tikitaka/src/views/HomeView.vue
+++ Vue/tikitaka/src/views/HomeView.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="home">
-    <div>
-      <movie-video-list
-        :movieList="nowPlayingMovieVideoList.movies"
-        :titleText="nowPlayingMovieVideoList.title"
-      />
+    <!-- 좌측(프로필) -->
+    <div id="profile-container">
+      <profile-list />
     </div>
-    <div id="container-box">
-      <br /><br />
-      <movie-list
-        :movieList="popularMovieList.movies"
-        :titleText="popularMovieList.title"
-      />
-      <movie-list
-        :movieList="nowPlayingMovieList.movies"
-        :titleText="nowPlayingMovieList.title"
-      />
+    <!-- 우측(영화출력) -->
+    <div id="movie-container">
+      <div>
+        <movie-video-list
+          :movieList="nowPlayingMovieVideoList.movies"
+          :titleText="nowPlayingMovieVideoList.title"
+        />
+      </div>
+      <div id="container-box">
+        <br /><br />
+        <movie-list
+          :movieList="popularMovieList.movies"
+          :titleText="popularMovieList.title"
+        />
+        <movie-list
+          :movieList="nowPlayingMovieList.movies"
+          :titleText="nowPlayingMovieList.title"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -23,9 +30,10 @@
 <script>
 import MovieList from "@/components/MovieList.vue"
 import MovieVideoList from "../components/MovieVideoList.vue"
+import ProfileList from "../components/ProfileList.vue"
 export default {
   name: "HomeView",
-  components: { MovieList, MovieVideoList },
+  components: { MovieList, MovieVideoList, ProfileList },
   computed: {
     popularMovieList() {
       return this.$store.state.popularMovieList
@@ -43,9 +51,11 @@ export default {
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap");
 #home {
+  display: flex;
   font-family: "Do Hyeon", sans-serif;
-  margin-left: 70px;
   margin-top: 70px;
+  width: 100vw;
+  background-color: white;
 }
 /* #trailer-box {
   display: absolute;
@@ -54,27 +64,27 @@ export default {
   z-index: 10;
 } */
 
-#container-box {
-  /* background-color: #004e66; */
-  background-color: white;
-  border-radius: 80px;
-  margin: -70px 20px;
-  padding: 20px;
-  display: absolute;
-  top: 10px;
+#profile-container {
+  width: 18%;
+  margin-right: 10px;
+  margin-left: 20px;
+  margin-top: 0;
 }
 
-/* #frame {
-  position: absolute;
-  top: 0;
-  left: 0;
+#movie-container {
+  width: 73%;
+  margin-left: 10px;
 }
-#info {
-  position: absolute;
-  bottom: 17px;
-  right: 10px;
-  z-index: 10;
-  height: 60px;
-  width: 140px;
-} */
+
+#container-box {
+  /* background-color: #004e66; */
+  background-color: transparent;
+  border-radius: 80px;
+  margin-top: -70px;
+  /* margin: -70px 20px; */
+  /* padding: 20px; */
+  display: absolute;
+  top: 10px;
+  /* width: 100%; */
+}
 </style>
